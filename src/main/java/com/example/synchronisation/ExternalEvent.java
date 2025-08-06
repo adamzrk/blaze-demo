fd89c2cd87bf3ddfeb7b447eeae8b34bd7bad54f
Adam Mazurek <Adam.mazurek@redocompass.com>
2025-08-06 10:01:32 +0200

    #1 Use reflection in configuration

--- src/main/java/com/example/synchronisation/ExternalEvent.java
+++ src/main/java/com/example/synchronisation/ExternalEvent.java
@@ -1,14 +1,27 @@
 package com.example.synchronisation;
 
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import org.springframework.context.ApplicationEvent;
 
+import java.time.Clock;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 
-@Value
 public class ExternalEvent extends ApplicationEvent {
 
     private final List<String> snapshots;
     private Instant instant;
+
+    public ExternalEvent(Object source) {
+        super(source);
+        this.snapshots = (ArrayList)source;
+    }
+
+    public ExternalEvent(Object source, Clock clock) {
+        super(source, clock);
+        this.snapshots = (ArrayList)source;
+    }
 }
